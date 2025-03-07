7d9d65e1d9ac8f9937bf18fd9d9694f771d66620
Jupiter <kjimin2123@gmail.com>
2025-03-07 21:04:38 +0900

    feat(auth): Implement login functionality
    - 로그인 기능을 추가하여 사용자가 이메일과 비밀번호로 인증할 수 있도록 하였습니다.
    - LocalAccountDto를 사용하여 요청 본문을 검증하며, 잘못된 이메일이나 비밀번호에 대해 BadRequestException을 발생시킵니다.

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAccountDto } from 'src/entities/dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() localAccountDto: LocalAccountDto) {
+    return await this.authService.login(localAccountDto);
+  }
 }
--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { LocalAccountDto } from 'src/entities/dtos/auth.dto';
 import { LocalAccount } from 'src/entities/local-account.entity';
 import { RefreshToken } from 'src/entities/refresh-token.entity';
+import { comparePassword } from 'src/utils/hash.util';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -12,4 +14,25 @@ export class AuthService {
     @InjectRepository(RefreshToken)
     private readonly refreshTokenRepository: Repository<RefreshToken>,
   ) {}
+
+  async login(localAccountDto: LocalAccountDto) {
+    const { email, password } = localAccountDto;
+
+    const localAccount = await this.localAccountRepository.findOne({
+      where: { email },
+      relations: ['user'],
+    });
+
+    if (!localAccount) {
+      throw new BadRequestException('등록되지 않은 이메일 혹은 틀린 비밀번호입니다.');
+    }
+
+    // 패스워드 검증
+    const isPasswordValid = await comparePassword(password, localAccount.password);
+    if (!isPasswordValid) {
+      throw new BadRequestException('등록되지 않은 이메일 혹은 틀린 비밀번호입니다.');
+    }
+
+    return localAccount.user;
+  }
 }
