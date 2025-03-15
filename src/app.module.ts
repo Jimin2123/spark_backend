8d496a623ec4601ac183150ef8b8b7e559ba28a9
Jupiter <88188840+Jimin2123@users.noreply.github.com>
2025-03-16 05:18:42 +0900

    Revert "8 redis settings"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeORM.config';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { UserModule } from './apis/user/user.module';
-import { AuthModule } from './apis/auth/auth.module';
-import { MqttModule } from './mqtt/mqtt.module';
-import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-    }),
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: 60, // 60초 동안
-          limit: 10, // 최대 10회 요청 허용
-        },
-      ],
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: typeORMConfig,
-    }),
-    UserModule,
-    AuthModule,
-    MqttModule,
-    RedisModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule } from '@nestjs/swagger';
-import swaggerConfig from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // API 문서화 설정
-  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, documentFactory());
-
-  // HTTP 보안 헤더 추가
-  app.use(helmet());
-
-  // 쿠키 파서 설정
-  app.use(cookieParser());
-
-  // 유효성 검사 파이프 설정
-  app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
