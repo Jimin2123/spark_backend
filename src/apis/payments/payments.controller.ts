6f030adf02d1e4f11f169e19ffd60b0ce4150730
Jupiter <kjimin2123@gmail.com>
2025-03-17 20:45:55 +0900

    feat(payments): Add Toss payment functionality
    - Toss 결제 요청을 처리하는 API를 추가했습니다.
    - 결제 수단, 금액, 주문명 등을 포함하는 DTO를 정의했습니다.
    - 결제 요청 시 필요한 인증 및 API 호출 로직을 구현했습니다.
    - 결제 수단을 열거형으로 정의하여 코드의 가독성을 높였습니다.

--- src/apis/payments/payments.controller.ts
+++ src/apis/payments/payments.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { TossPaymentDto } from 'src/entities/dtos/payments.dto';
 
 @Controller('payments')
+@ApiBearerAuth()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('/toss/request')
+  @UseGuards(JwtAuthGuard)
+  async requestPayment(@CurrentUser() userId: string, @Body() tossPaymentDto: TossPaymentDto) {
+    const result = await this.paymentsService.createTossPayment(userId, tossPaymentDto);
+    return { result };
+  }
 }
--- src/apis/payments/payments.service.ts
+++ src/apis/payments/payments.service.ts
@@ -1,4 +1,55 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { randomUUID } from 'crypto';
+import { firstValueFrom } from 'rxjs';
+import { UserService } from '../user/user.service';
+import { TossPaymentDto } from 'src/entities/dtos/payments.dto';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  private readonly tossSecretKey: string;
+  private readonly tossClientKey: string;
+  private readonly tossSuccessUrl: string;
+  private readonly tossFailUrl: string;
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.tossSecretKey = this.configService.get<string>('TOSS_SECRET_KEY', '');
+    this.tossClientKey = this.configService.get<string>('TOSS_CLIENT_KEY', '');
+    this.tossSuccessUrl = this.configService.get<string>('TOSS_SUCCESS_URL', '');
+    this.tossFailUrl = this.configService.get<string>('TOSS_FAIL_URL', '');
+  }
+
+  async createTossPayment(userId: string, paylaod: TossPaymentDto) {
+    const { amount, method, orderName } = paylaod;
+    const authHeader = `Basic ${Buffer.from(this.tossSecretKey + ':', 'utf-8').toString('base64')}`;
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: authHeader,
+    };
+    const user = await this.userService.findUserById(userId);
+
+    const data = {
+      method, // 결제수단
+      amount, // 결제할 금액
+      orderId: randomUUID(), // 주문번호
+      orderName, // 주문명
+      customerName: user.name, // 주문자 이름
+      successUrl: this.tossSuccessUrl,
+      failUrl: this.tossFailUrl,
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post('https://api.tosspayments.com/v1/payments', data, { headers }),
+      );
+      return response.data; // checkoutPage URL 반환
+    } catch (error) {
+      console.error('Toss Payment API Error:', error.response?.data || error.message);
+    }
+  }
+}
