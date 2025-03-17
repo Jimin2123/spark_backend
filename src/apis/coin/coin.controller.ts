791291eecec75a80927d22e2a6b8153131c25c3b
Jupiter <kjimin2123@gmail.com>
2025-03-18 01:40:28 +0900

    docs(coin): Add Swagger documentation for coin APIs
    - 코인 사용 및 잔액 조회 API에 대한 Swagger 문서를 추가했습니다.
    - 이를 통해 API 사용자가 각 기능의 목적과 요청/응답 형식을 쉽게 이해할 수 있습니다.

--- src/apis/coin/coin.controller.ts
+++ src/apis/coin/coin.controller.ts
@@ -4,6 +4,7 @@ import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { SpendCoinDto } from 'src/entities/dtos/coin.dto';
+import { SwaggerGetBalance, SwaggerSpendCoin } from 'src/common/docs/coin.swagger';
 
 @Controller('coin')
 @ApiBearerAuth()
@@ -11,12 +12,14 @@ export class CoinController {
   constructor(private readonly coinService: CoinService) {}
 
   @Get()
+  @SwaggerGetBalance()
   @UseGuards(JwtAuthGuard)
   async getBalance(@CurrentUser() userId: string) {
     return this.coinService.getBalance(userId);
   }
 
   @Post()
+  @SwaggerSpendCoin()
   @UseGuards(JwtAuthGuard)
   async spendCoin(@CurrentUser() userId: string, @Body() spendCoinDto: SpendCoinDto) {
     return this.coinService.spendCoin(userId, spendCoinDto);
